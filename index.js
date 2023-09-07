5717139b70652cf66fc9df0349024672f714fabd
Fiote <brenoft14@gmail.com>
2023-09-07 18:19:02 -0300

    aparecer valor

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const btn1 = document.querySelector('#b4')
-btn1.addEventListener('click',()=>{
-    window.location = 'tenis.html'
-})
+const atual = document.querySelector('#atual')
+const input = document.querySelector('#hj')
+const atualiza = document.querySelector('#atualiza')
+
+function atualizar (){
+    const valorAT = document.querySelector('#hj').value
+    atual.textContent = 'Voce esta com: R$:'+valorAT
+}
+
+function salvar (){
+    localStorage.valor = document.querySelector('#hj').value
+    console.log(localStorage.valor)
+}
