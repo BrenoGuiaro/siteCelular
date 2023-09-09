072e6e9ba5efdbeb130e9b1ba870f6eb2117bfd8
Fiote <brenoft14@gmail.com>
2023-09-09 06:12:04 -0300

    ver como esta no celular

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const atual = document.querySelector('#atual')
-const input = document.querySelector('#hj')
-const atualiza = document.querySelector('#atualiza')
-
-function atualizar (){
-    const valorAT = document.querySelector('#hj').value
-    atual.textContent = 'Voce esta com: R$:'+valorAT
-}
-
-function salvar (){
-    localStorage.valor = document.querySelector('#hj').value
-    console.log(localStorage.valor)
-}
+const voltar  = document.querySelector('#logo')
+voltar.addEventListener('click',()=>{
+    window.location = 'index.html'
+})
